2696ada8db09f5e57a87f9cae5d3333f2c8eeb9a
sezalcru <cesarcruz91@gmail.com>
2014-10-11 23:52:08 -0400

    Added venmo charge and venmo webhook

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request
 from twilio import twiml
+import requests
+import venmo
 
 app = Flask(__name__)
+
+import venmo_webhook
+
 
 menu = {
   'CL8': {
@@ -18,23 +23,27 @@ menu = {
   }
 }
 
-@app.route('/', methods=['GET', 'POST'])
-def hello():
-  if request.method == 'POST':
-    r = twiml.Response()
-    order = request.form['Body']
-    if menu.has_key(order):
-      r.message('Ordered %s' % menu[order]['name'])
-    elif order == 'MENU':
-      m = ''
-      for key,value in menu.iteritems():
-        m += 'Name: ' + value['name'] + ' Price: $' + str(value['price']) + ' Code: ' + key + '\n\n'
-      r.message(m)
-    else:
-      r.message('No item for that key.\n\n Send MENU for list of items.')
-    return str(r)
 
-  return 'Hola'
+# Method to handle text messages with orders
+def handle_order(order):
+  r = twiml.Response()
+  if menu.has_key(order):
+    # TODO: Send paypal invoice for item here
+    r.message('Ordered %s' % menu[order]['name']) # This should be the invoice info
+  elif order == 'MENU':
+    m = ''
+    for key,value in menu.iteritems():
+      m += 'Name: ' + value['name'] + ' Price: $' + str(value['price']) + ' Code: ' + key + '\n\n'
+    r.message(m)
+  else:
+    r.message('No item for that key.\n\n Send MENU for list of items.')
+  return str(r)
+
+@app.route('/', methods=['POST'])
+def hello():
+  order = request.form['Body']
+  return handle_order(order)
+
 
 if __name__ == '__main__':
   app.run(debug=True)
